55ace2f2568cc2b5f6d1f40b061106907d1b26ef
CeleryAdams <sallyceleryadams@gmail.com>
2024-05-21 21:51:25 -0400

    prototype infinite scroll background with parallax

---
+++ Cat Game/Assets/Scripts/MoveBackground.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    private float spriteLength;
+    private float startPosition;
+    private GameObject camera;
+    private GameObject defaultSprite;
+    [SerializeField] private float parallaxAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GameObject.Find("Main Camera");
+        defaultSprite = GameObject.Find("trees");
+        startPosition = transform.position.x;
+        spriteLength = defaultSprite.GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = (camera.transform.position.x * (1 - parallaxAmount));
+        float distance = (camera.transform.position.x * parallaxAmount);
+
+        transform.position = new Vector3(startPosition + distance, transform.position.y, transform.position.z);
+
+        if (temp > startPosition + spriteLength * 0.5)
+        {
+            startPosition += spriteLength;
+        }
+        else if (temp < startPosition - spriteLength * 0.5)
+        {
+            startPosition -= spriteLength;
+        }
+
+    }
+}
